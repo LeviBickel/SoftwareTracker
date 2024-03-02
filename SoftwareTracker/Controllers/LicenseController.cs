bceb3c66f845e7649dd13b880fcba3d70876c5c5
Levi Bickel <levibickel@gmail.com>
2024-03-02 16:34:25 -0500

    Scaffolded the controllers for both of our new models and also added the associated views.

---
+++ SoftwareTracker/Controllers/LicenseController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SoftwareTracker.Data;
+using SoftwareTracker.Models;
+
+namespace SoftwareTracker.Controllers
+{
+    public class LicenseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LicenseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: License
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Licenses.ToListAsync());
+        }
+
+        // GET: License/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var licenseModel = await _context.Licenses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (licenseModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(licenseModel);
+        }
+
+        // GET: License/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: License/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Manufacturer,SoftwareTitle,AssignedServer,PurchaseOrder,PurchaseDate,LicenseType,Support,SupportExp,AmountofKeys,UsedKeys,RemainingKeys,LicenseKey")] LicenseModel licenseModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(licenseModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(licenseModel);
+        }
+
+        // GET: License/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var licenseModel = await _context.Licenses.FindAsync(id);
+            if (licenseModel == null)
+            {
+                return NotFound();
+            }
+            return View(licenseModel);
+        }
+
+        // POST: License/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Manufacturer,SoftwareTitle,AssignedServer,PurchaseOrder,PurchaseDate,LicenseType,Support,SupportExp,AmountofKeys,UsedKeys,RemainingKeys,LicenseKey")] LicenseModel licenseModel)
+        {
+            if (id != licenseModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(licenseModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LicenseModelExists(licenseModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(licenseModel);
+        }
+
+        // GET: License/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var licenseModel = await _context.Licenses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (licenseModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(licenseModel);
+        }
+
+        // POST: License/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var licenseModel = await _context.Licenses.FindAsync(id);
+            if (licenseModel != null)
+            {
+                _context.Licenses.Remove(licenseModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LicenseModelExists(int id)
+        {
+            return _context.Licenses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SoftwareTracker/Controllers/UserAdministrationController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SoftwareTracker.Data;
+using SoftwareTracker.Models;
+
+namespace SoftwareTracker.Controllers
+{
+    public class UserAdministrationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserAdministrationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserAdministration
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.userAdministration.ToListAsync());
+        }
+
+        // GET: UserAdministration/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userAdministration = await _context.userAdministration
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userAdministration == null)
+            {
+                return NotFound();
+            }
+
+            return View(userAdministration);
+        }
+
+        // GET: UserAdministration/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserAdministration/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserName,UserEmail,LockOutEndDate,CanLockout,Role")] UserAdministration userAdministration)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userAdministration);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userAdministration);
+        }
+
+        // GET: UserAdministration/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userAdministration = await _context.userAdministration.FindAsync(id);
+            if (userAdministration == null)
+            {
+                return NotFound();
+            }
+            return View(userAdministration);
+        }
+
+        // POST: UserAdministration/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,UserEmail,LockOutEndDate,CanLockout,Role")] UserAdministration userAdministration)
+        {
+            if (id != userAdministration.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userAdministration);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserAdministrationExists(userAdministration.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userAdministration);
+        }
+
+        // GET: UserAdministration/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userAdministration = await _context.userAdministration
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userAdministration == null)
+            {
+                return NotFound();
+            }
+
+            return View(userAdministration);
+        }
+
+        // POST: UserAdministration/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var userAdministration = await _context.userAdministration.FindAsync(id);
+            if (userAdministration != null)
+            {
+                _context.userAdministration.Remove(userAdministration);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserAdministrationExists(string id)
+        {
+            return _context.userAdministration.Any(e => e.Id == id);
+        }
+    }
+}
