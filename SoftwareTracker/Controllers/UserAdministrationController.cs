de1bc02058288ac784a7db9da44413ffd3257d6e
Levi Bickel <levibickel@gmail.com>
2024-03-03 19:07:17 -0500

    Added the required logic for the User Administration Contoller.

--- SoftwareTracker/Controllers/UserAdministrationController.cs
+++ SoftwareTracker/Controllers/UserAdministrationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -15,16 +16,23 @@ namespace SoftwareTracker.Controllers
     public class UserAdministrationController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public UserAdministrationController(ApplicationDbContext context)
+        public UserAdministrationController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: UserAdministration
         public async Task<IActionResult> Index()
         {
-            return View(await _context.userAdministration.ToListAsync());
+            List<UserAdministration> usersToView = new List<UserAdministration>();
+            foreach (var user in _context.Users)
+            {
+                usersToView.Add(await TranslateUserToView(user));
+            }
+            return View(usersToView);
         }
 
         // GET: UserAdministration/Details/5
@@ -35,14 +43,15 @@ namespace SoftwareTracker.Controllers
                 return NotFound();
             }
 
-            var userAdministration = await _context.userAdministration
+            var user = await _context.Users
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (userAdministration == null)
+            var translatedUser = await TranslateUserToView(user);
+            if (translatedUser == null || user == null)
             {
                 return NotFound();
             }
 
-            return View(userAdministration);
+            return View(translatedUser);
         }
 
         // GET: UserAdministration/Create
@@ -54,18 +63,18 @@ namespace SoftwareTracker.Controllers
         // POST: UserAdministration/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserName,UserEmail,LockOutEndDate,CanLockout,Role")] UserAdministration userAdministration)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(userAdministration);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(userAdministration);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,UserName,UserEmail,LockOutEndDate,CanLockout,Role")] UserAdministration userAdministration)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(userAdministration);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(userAdministration);
+        //}
 
         // GET: UserAdministration/Edit/5
         public async Task<IActionResult> Edit(string id)
@@ -75,12 +84,13 @@ namespace SoftwareTracker.Controllers
                 return NotFound();
             }
 
-            var userAdministration = await _context.userAdministration.FindAsync(id);
-            if (userAdministration == null)
+            var user = await _context.Users.FindAsync(id);
+            var translatedUser = await TranslateUserToView(user);
+            if (user == null || translatedUser == null)
             {
                 return NotFound();
             }
-            return View(userAdministration);
+            return View(translatedUser);
         }
 
         // POST: UserAdministration/Edit/5
@@ -90,7 +100,8 @@ namespace SoftwareTracker.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,UserEmail,LockOutEndDate,CanLockout,Role")] UserAdministration userAdministration)
         {
-            if (id != userAdministration.Id)
+            IdentityUser user = await _context.Users.FindAsync(id);
+            if (id == null || id != user.Id)
             {
                 return NotFound();
             }
@@ -99,7 +110,18 @@ namespace SoftwareTracker.Controllers
             {
                 try
                 {
-                    _context.Update(userAdministration);
+                    user.UserName = userAdministration.UserName;
+                    user.NormalizedEmail = userAdministration.UserEmail;
+                    user.LockoutEnd = userAdministration.LockOutEndDate;
+                    user.LockoutEnabled = userAdministration.CanLockout;
+
+                    //Modify the users role if needed:
+                    if (!_userManager.GetRolesAsync(user).Result.Contains(userAdministration.Role))
+                    {
+                        await _userManager.RemoveFromRoleAsync(user, _userManager.GetRolesAsync(user).Result.First());
+                        await _userManager.AddToRoleAsync(user, userAdministration.Role);
+                    }
+                    _context.Update(user);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -126,14 +148,15 @@ namespace SoftwareTracker.Controllers
                 return NotFound();
             }
 
-            var userAdministration = await _context.userAdministration
+            var user = await _context.Users
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (userAdministration == null)
+            var translatedUser = await TranslateUserToView(user);
+            if (user == null || translatedUser == null)
             {
                 return NotFound();
             }
 
-            return View(userAdministration);
+            return View(translatedUser);
         }
 
         // POST: UserAdministration/Delete/5
@@ -141,14 +164,33 @@ namespace SoftwareTracker.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
         {
-            var userAdministration = await _context.userAdministration.FindAsync(id);
-            if (userAdministration != null)
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
             {
-                _context.userAdministration.Remove(userAdministration);
+                _context.Users.Remove(user);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<UserAdministration> TranslateUserToView(IdentityUser user)
+        {
+            string userRole = "Users";
+            if (await _userManager.IsInRoleAsync(user, "Administrators"))
+            {
+                userRole = "Administrators";
+            }
+            UserAdministration translatedUser = new UserAdministration
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                UserEmail = user.NormalizedEmail,
+                CanLockout = user.LockoutEnabled,
+                LockOutEndDate = user.LockoutEnd,
+                Role = userRole
+            };
+            return translatedUser;
         }
 
         private bool UserAdministrationExists(string id)
