4f29329ffb79213efe19898c581b368d2a2bb9e7
Levi Bickel <levibickel@gmail.com>
2024-04-10 18:01:13 -0400

    Reorganized the AKeylessHelper and added a function to retrieve the sendgrid token.

--- SoftwareTracker/Data/AkeylessHelper.cs
+++ SoftwareTracker/Data/AkeylessHelper.cs
@@ -2,6 +2,7 @@
 using akeyless.Api;
 using akeyless.Model;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SoftwareTracker.Data
 {
@@ -9,39 +10,45 @@ namespace SoftwareTracker.Data
     {
         //string token = "";
         //V2Api instance = null;
-
+        static V2Api instance;
         public static string RetrieveEncryptionKey()
         {
-            Configuration config = new Configuration();
-            config.BasePath = "https://api.akeyless.io";
-            var instance = new V2Api(config);
-
-            var authBody = new Auth(accessId: Environment.GetEnvironmentVariable("STAccessID"), accessKey: Environment.GetEnvironmentVariable("STAccessKey"));
-            AuthOutput authResult = instance.Auth(authBody);
-            string token = authResult.Token;
+            string token = AuthenticateToAKeyless();
             List<String> secrets = new List<String>();
             secrets.Add("SoftwareTracker/EncryptionKey");
             var getSecretValueBody = new GetSecretValue(names: secrets, token: token);
             Dictionary<string, string> getSecretValueResult = instance.GetSecretValue(getSecretValueBody);
-            //Console.WriteLine(getSecretValueResult["netcore"]);
             return getSecretValueResult["SoftwareTracker/EncryptionKey"];
         }
 
         public static string RetrieveConnectionString()
         {
-            Configuration config = new Configuration();
-            config.BasePath = "https://api.akeyless.io";
-            var instance = new V2Api(config);
-
-            var authBody = new Auth(accessId: Environment.GetEnvironmentVariable("STAccessID"), accessKey: Environment.GetEnvironmentVariable("STAccessKey"));
-            AuthOutput authResult = instance.Auth(authBody);
-            string token = authResult.Token;
+            string token = AuthenticateToAKeyless();
             List<String> secrets = new List<String>();
             secrets.Add("SoftwareTracker/ConnectionString");
             var getSecretValueBody = new GetSecretValue(names: secrets, token: token);
             Dictionary<string, string> getSecretValueResult = instance.GetSecretValue(getSecretValueBody);
-            //Console.WriteLine(getSecretValueResult["netcore"]); 
             return getSecretValueResult["SoftwareTracker/ConnectionString"];
+        }
+        public static string RetrieveSendGridToken()
+        {
+            string token = AuthenticateToAKeyless();
+            List<String> secrets = new List<String>();
+            secrets.Add("SoftwareTracker/SendGridAPI");
+            var getSecretValueBody = new GetSecretValue(names: secrets, token: token);
+            Dictionary<string, string> getSecretValueResult = instance.GetSecretValue(getSecretValueBody);
+            return getSecretValueResult["SoftwareTracker/SendGridAPI"];
+        }
+        private static string AuthenticateToAKeyless()
+        {
+            Configuration config = new Configuration();
+            config.BasePath = "https://api.akeyless.io";
+            instance = new V2Api(config);
+
+            var authBody = new Auth(accessId: Environment.GetEnvironmentVariable("STAccessID"), accessKey: Environment.GetEnvironmentVariable("STAccessKey"));
+            AuthOutput authResult = instance.Auth(authBody);
+            string token = authResult.Token;
+            return token;
         }
     }
 }
