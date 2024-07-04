cfc23b3dcd10a5e0f66ed9558626fec970f64faa
Levi Bickel <levibickel@gmail.com>
2024-07-04 09:57:01 -0400

    Updated the notifcations for both license expiration and support expiration. Also provided a toggle for the user to select what they want to be notified for.

---
+++ SoftwareTracker/Data/Migrations/20240704130717_notifications.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftwareTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class notifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NotifyOnLicExp",
+                table: "Licenses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "NotifyOnSupExp",
+                table: "Licenses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotifyOnLicExp",
+                table: "Licenses");
+
+            migrationBuilder.DropColumn(
+                name: "NotifyOnSupExp",
+                table: "Licenses");
+        }
+    }
+}
