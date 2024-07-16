86a37104b565d14e364c445451d213fdd4ea6de5
Levi Bickel <levibickel@gmail.com>
2024-07-16 11:34:32 -0400

    Initial implementation completed. Views need refactored to meet the same theme. Also need to look at who we want to have access to this portion and how that navigation will look.

---
+++ SoftwareTracker/Data/Migrations/20240618192930_archival.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftwareTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class archival : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Archival",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SoftwareTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssignedServer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PurchaseOrder = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LicenseType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LicenseExp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Support = table.Column<bool>(type: "bit", nullable: false),
+                    SupportExp = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AmountofKeys = table.Column<int>(type: "int", nullable: false),
+                    UsedKeys = table.Column<int>(type: "int", nullable: false),
+                    RemainingKeys = table.Column<int>(type: "int", nullable: false),
+                    LicenseKey = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AddedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notified = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Archival", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Archival");
+        }
+    }
+}
